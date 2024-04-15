2aef192a61c72fd496801689550c8808e9920e65
Sanjay3739 <makwanasanjaylm@gmail.com>
2024-04-15 17:28:25 +0530

    api helper add

--- app/Helpers/ApiHelper.php
+++ app/Helpers/ApiHelper.php
@@ -2,10 +2,13 @@
 
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\Config;
+
 class ApiHelper
 {
-    public static function response($success, $data = [], $message = null, $status = 200)
+    public static function response($success, $data = [], $messageKey = null, $status = null)
     {
+        $message = Config::get('constant.' . $messageKey);
         return [
             'success' => $success,
             'data' => $data,
